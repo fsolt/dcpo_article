8244329083567cc89173daf08ecdff562646670d
Frederick Solt <frederick-solt@uiowa.edu>
2019-11-05 14:58:33 -0600

    include format_dcpo call

--- data/kfold/dcpo_kfold.R
+++ data/kfold/dcpo_kfold.R
@@ -4,6 +4,10 @@ library(DCPO)
 
 demsup <- read_csv(system.file("extdata", "all_data_demsupport.csv", package = "DCPOtools"))
 
+demsup_data <- format_dcpo(demsup %>% with_min_yrs(3),
+                                          scale_q = "church_21",
+                                          scale_cp = 2)
+
 dcpo_demsup_kfold <- purrr::map(1:10, function(x) {
                            dcpo_xvt(demsup_data,
                                       fold_number = x, # number_of_folds = 10 is the default
